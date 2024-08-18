29ef2e39933303900f0fa2628f4cb50c22125167
Likith P <80813680+likith5@users.noreply.github.com>
2024-08-18 14:53:35 +0530

    added environemnet variables

--- src/config/firebase_config.js
+++ src/config/firebase_config.js
@@ -1,19 +1,16 @@
 // Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
 import { getFirestore } from "firebase/firestore";
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
 
 // Your web app's Firebase configuration
-// For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: "AIzaSyC8VtuxVUyR7aZAufBeQtBGyu9fIZ2Qjmc",
-  authDomain: "portfolio-ce93e.firebaseapp.com",
-  projectId: "portfolio-ce93e",
-  storageBucket: "portfolio-ce93e.appspot.com",
-  messagingSenderId: "96284714494",
-  appId: "1:96284714494:web:58edd03208fce7c2240caf",
-  measurementId: "G-8CK1VHZFTE"
+  apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
+  authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.REACT_APP_FIREBASE_APP_ID,
+  measurementId: process.env.REACT_APP_FIREBASE_MEASUREMENT_ID,
 };
 
 // Initialize Firebase
